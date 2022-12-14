234a121a9ce3dce985218fec84db9fe92e13d8b9
Tailine Maria <tailine.oliver@gmail.com>
2022-12-14 13:57:54 -0300

    tela home Dio

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+import bannerImage from '../../assets/banner.png'
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import {Container, TextContent, Title, TitleHighlight }from './styles'
+
+const Home = ()=>{
+    return (<>
+        <Header/>
+        <Container>
+            <div>
+                <Title>
+                    <TitleHighlight>
+                        Implemente
+                        <br />
+                    </TitleHighlight>
+                    o seu futuro global agora!
+                </Title>
+                <TextContent>
+                    Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
+                </TextContent>
+                <Button title="Começar agora" variant ="secondary" onClick={()=>null}/>
+
+            </div>
+            <div>
+                <img src={bannerImage} alt ="Imagem Principal" />
+            </div>
+        </Container>
+        
+   
+    </>)
+}
+
+export { Home }
